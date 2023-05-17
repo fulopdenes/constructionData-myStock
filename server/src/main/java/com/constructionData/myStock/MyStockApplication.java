1217684082a9239e276f9dd6e050b8f44a3d6248
fulopdenes <denes.fulopp@gmail.com>
2023-05-17 12:57:05 +0200

    config security configuration

--- server/src/main/java/com/constructionData/myStock/MyStockApplication.java
+++ server/src/main/java/com/constructionData/myStock/MyStockApplication.java
@@ -2,12 +2,12 @@ package com.constructionData.myStock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class MyStockApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MyStockApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MyStockApplication.class, args);
+    }
 
 }
