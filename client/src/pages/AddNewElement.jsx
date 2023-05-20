514fba41cbe0f91b5a08d9924c103b6af2ffdc51
fulopdenes <denes.fulopp@gmail.com>
2023-05-20 14:32:46 +0200

    add fetch and specify necessary details parameters

--- client/src/pages/AddNewElement.jsx
+++ client/src/pages/AddNewElement.jsx
@@ -3,14 +3,15 @@ import {Button, Card, TextField} from "@mui/material"
 import {useNavigate} from "react-router-dom";
 
 const AddNewElement = () => {
+    const navigate = useNavigate();
     const [product, setProduct] = useState({
-        name:"",
         category:"",
-        techCode:"",
+        productTechCode:"",
         quantity: "",
         quantityType: "",
         relatedUnit: "",
-        roomName: ""
+        roomNameOfInstallation: "",
+        productName: ""
     });
     const navigator = useNavigate();
 
@@ -21,9 +22,7 @@ const AddNewElement = () => {
     }
 
     const handleSubmit = async () => {
-
-        console.log(product)
-        const res = await fetch("http://localhost:8080/api/products/", {
+        const res = await fetch(`http://localhost:8080/products/new`, {
             method: "POST",
             headers: {
             "Content-Type": "application/json"
@@ -51,14 +50,16 @@ const AddNewElement = () => {
                 gap: 4,
                 borderRadius: "15px"
             }} elevation={10}>
-                <TextField id="outlined-basic" label="Name" variant="outlined" type={"text"} name={"name"} onChange={handleChange} value={product.name} required={true} />
+                New Product Details:
+                <TextField id="outlined-basic" label="Name" variant="outlined" type={"text"} name={"productName"} onChange={handleChange} value={product.productName} required={true} />
                 <TextField id="outlined-basic" label="Category" variant="outlined" type={"text"} name={"category"} onChange={handleChange} value={product.category} required={true} />
-                <TextField id="outlined-basic" label="TechCode" variant="outlined" type={"text"} name={"techCode"} onChange={handleChange} value={product.techCode} required={true}/>
+                <TextField id="outlined-basic" label="TechCode" variant="outlined" type={"text"} name={"productTechCode"} onChange={handleChange} value={product.productTechCode} required={true}/>
                 <TextField id="outlined-basic" label="Quantity" variant="outlined" type={"number"} name={"quantity"} onChange={handleChange} value={product.quantity} required={true}/>
                 <TextField id="outlined-basic" label="Quantity Type" variant="outlined" type={"text"} name={"quantityType"} onChange={handleChange} value={product.quantityType} required={true}/>
                 <TextField id="outlined-basic" label="Related Unit" variant="outlined" type={"text"} name={"relatedUnit"} onChange={handleChange} value={product.relatedUnit} required={true}/>
-                <TextField id="outlined-basic" label="Room Name" variant="outlined" type={"text"} name={"roomName"} onChange={handleChange} value={product.roomName} required={true}/>
+                <TextField id="outlined-basic" label="Room Name" variant="outlined" type={"text"} name={"roomNameOfInstallation"} onChange={handleChange} value={product.roomNameOfInstallation} required={true}/>
                 <Button variant={"contained"} onClick={handleSubmit}>Add New Product</Button>
+                <Button variant={"outlined"} color="error" onClick={() => navigate(-1)} >Back</Button>
             </Card>
             {/*<FileUploader/>*/}
         </>
