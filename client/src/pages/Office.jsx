10872d347d5a7b5999d827032fb7a4fe3842b961
fulopdenes <denes.fulopp@gmail.com>
2023-05-20 14:50:18 +0200

    setup icon links at office page

--- client/src/pages/Office.jsx
+++ client/src/pages/Office.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Box, SpeedDial, SpeedDialIcon} from "@mui/material";
 import ExportDefaultToolbar from "../components/Grid/ExportDefaultToolbar";
 import SpeedDialAction from "@mui/material/SpeedDialAction";
@@ -7,6 +7,10 @@ import DocumentScannerIcon from '@mui/icons-material/DocumentScanner';
 import AddCircleOutlineRoundedIcon from '@mui/icons-material/AddCircleOutlineRounded';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
+import CircularIndeterminateLoading from "../components/CircularIndeterminateLoading";
+import {red} from "@mui/material/colors";
+import {Link} from "react-router-dom";
+
 
 const StyledSpeedDial = styled(SpeedDial)(({ theme }) => ({
     position: 'absolute',
@@ -18,16 +22,49 @@ const StyledSpeedDial = styled(SpeedDial)(({ theme }) => ({
         top: theme.spacing(2),
         left: theme.spacing(2),
     },
+    "& svg": {
+        display: "block"
+    },
 }));
 
+const withLink = (to, children) => <Link to={to}>{children}</Link>;
+
 const actions = [
-    { icon: <EditIcon />, name: 'Edit Table' },
-    { icon: <DeleteIcon />, name: 'Delete Element' },
-    { icon: <AddCircleOutlineRoundedIcon />, name: 'Add new element' },
-    { icon: <DocumentScannerIcon />, name: 'Scan document' },
+    { icon: withLink("/", <EditIcon sx={{ color: "#000" }} />), name: 'Edit Table'},
+    { icon: withLink("/delete", <DeleteIcon sx={{ color: red[500] }} />), name: 'Delete Element'},
+    { icon: withLink("/new", <AddCircleOutlineRoundedIcon color="primary"/>), name: 'Add new element' },
+    { icon: <DocumentScannerIcon color="disabled"/>, name: 'Scan document' }
 ];
 
-const Home = () => {
+const fetchProducts = (signal) => {
+    return fetch(`http://localhost:8080/products/all`, { signal }).then((res) => res.json());
+};
+
+const Office = () => {
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        const controller = new AbortController();
+
+        fetchProducts(controller.signal)
+            .then((products) => {
+                setLoading(false);
+                setData(products);
+            })
+            .catch((error) => {
+                if (error.name !== "AbortError") {
+                    setData(null);
+                    throw error;
+                }
+            });
+
+        return () => controller.abort();
+    }, []);
+
+    if (loading) {
+        return <CircularIndeterminateLoading />;
+    }
     return (
         <>
         {/*    <Card sx={{*/}
@@ -40,14 +77,14 @@ const Home = () => {
         {/*    // gap: 0,*/}
         {/*    borderRadius: "15px"*/}
         {/*}} elevation={10}>*/}
-
-                <ExportDefaultToolbar/>
+                <ExportDefaultToolbar products={data}/>
                 {/*<Button variant={"contained"}><SpeedDial ariaLabel={"SpeedDial basic example"}></SpeedDial></Button>*/}
                 {/*<SpeedDial*/}
                 {/*    ariaLabel="SpeedDial basic example"*/}
                 {/*    icon={<SpeedDialIcon />}></SpeedDial>*/}
                 {/*<PlaygroundSpeedDial/>*/}
                 <Box sx={{ position: 'relative', mt: 0, height: 80 }}>
+
                     <StyledSpeedDial
                         ariaLabel="SpeedDial playground example"
                         // hidden={hidden}
@@ -62,6 +99,21 @@ const Home = () => {
                             />
                         ))}
                     </StyledSpeedDial>
+
+                {/*    <SpeedDial*/}
+                {/*        ariaLabel="Save Feedback"*/}
+                {/*        icon={<SpeedDialIcon icon={<EditIcon />} openIcon={<EditIcon />} />}*/}
+                {/*        onClick={() => fabAction()}*/}
+                {/*        FabProps={{*/}
+                {/*          onClick: () => fabAction()*/}
+                {/*        }}*/}
+                {/*    open={true}*/}
+                {/*    direction="left">*/}
+                {/*    <SpeedDialAction*/}
+                {/*        icon={<EditIcon />}*/}
+                {/*        tooltipTitle="Add Point"*/}
+                {/*        onClick={() => fabActionAdd()} />*/}
+                {/*</SpeedDial>*/}
                 </Box>
                 {/*<SpeedDialAction*/}
                 {/*    key={action.name}*/}
@@ -73,4 +125,4 @@ const Home = () => {
     )
 }
 
-export default Home;
+export default Office;
