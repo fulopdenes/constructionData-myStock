d74e1e920c018d1a3996dd463d1e8a989dc95028
fulopdenes <denes.fulopp@gmail.com>
2023-05-20 14:48:26 +0200

    add setup fetch

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -10,8 +10,20 @@ const Login = () => {
         setUser ({...user, [name]: value});
     }
 
-    const handleSubmit = () => {
-        console.log(user)
+    const handleSubmit = async () => {
+        const res = await fetch(`http://localhost:8080/users/`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(user)
+        })
+        const data = await res.json()
+        if (res.ok) {
+            navigator("/login");
+        } else {
+            console.log(data);
+        }
     }
     return (
         <Card sx={{
@@ -25,12 +37,12 @@ const Login = () => {
             borderRadius: "15px"
         }} elevation={10}>
             <CardContent sx={{m: 0}}>
-                <Typography gutterButton variant={"h4"} component={"div"} sx={{m: 0}}>
+                <Typography variant={"h4"} component={"div"} sx={{m: 0}}>
                     Login Here!
                 </Typography>
             </CardContent>
             <TextField id="outlined-basic" label="Email" variant="outlined" type={"email"} name={"email"} onChange={handleChange} value={user.email}/>
-            <TextField id="outlined-basic" label="Password" variant="outlined" type={"password"} name={"password"}onChange={handleChange} value={user.password}/>
+            <TextField id="outlined-basic" label="Password" variant="outlined" type={"password"} name={"password"} onChange={handleChange} value={user.password}/>
             <Button variant={"contained"} onClick={handleSubmit}>Login</Button>
         </Card>
     )
