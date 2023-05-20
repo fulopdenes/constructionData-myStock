299195c2946d8f95d0a5ec34f75cb2b7f7399190
fulopdenes <denes.fulopp@gmail.com>
2023-05-20 14:51:34 +0200

    fix

--- server/src/main/java/com/constructionData/myStock/controller/TestController.java
+++ server/src/main/java/com/constructionData/myStock/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.constructionData.myStock.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@CrossOrigin(origins = {"http://192.168.0.17:3000", "http://localhost:3000"})
 public class TestController {
 
     @GetMapping("/")
