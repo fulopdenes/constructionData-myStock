75bec4b0e5eff03032033edac71e385541ad648d
fulopdenes <denes.fulopp@gmail.com>
2023-05-21 21:33:23 +0200

    remove unused imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Register from "./pages/Register";
 import NavBar from "./components/NavBar";
 import {Container} from "@mui/material";
 import AddNewElement from "./pages/AddNewElement";
 import Office from "./pages/Office";
 import OfficeEdit from "./pages/OfficeEdit";
 import OnSite from "./pages/OnSite";
-import RegisterPage from "./pages/RegisterPage";
 
 const App = () => {
     return (
