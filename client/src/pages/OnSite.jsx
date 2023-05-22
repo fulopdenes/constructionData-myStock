ed7de2e6e82d0b590e35372dbdb55a540cbe5d73
fulopdenes <denes.fulopp@gmail.com>
2023-05-22 05:27:22 +0200

    add StorageLocationHandler.jsx and SearchBarAndValueHandler.jsx platform

--- client/src/pages/OnSite.jsx
+++ client/src/pages/OnSite.jsx
@@ -1,29 +1,15 @@
 import React from 'react';
-import {Button, Card, CardContent, Typography} from "@mui/material"
+import StorageLocationHandler from "../components/StorageLocationHandler";
+
+import SearchBarAndValueHandler from "../components/SearchBarAndValueHandler";
 
 const OnSite = () => {
-    return (
-        <Card sx={{
-            p: 3,
-            py: 5,
-            maxWidth: "550px",
-            margin: "50px auto",
-            display: "flex",
-            flexDirection: "column",
-            gap: 4,
-            borderRadius: "15px"
-        }} elevation={10}>
-            <CardContent sx={{m: 0}}>
-                <Typography variant={"h4"} component={"div"} sx={{m: 0}}>
-                    Get element of:
-                </Typography>
-            </CardContent>
-            Filter
-            based on 1 condition . select column
-            list al card with confirmation button.
 
-            <Button variant={"contained"}>Search</Button>
-        </Card>
+    return (
+        <>
+            <StorageLocationHandler/>
+            <SearchBarAndValueHandler/>
+        </>
     )
 }
 
