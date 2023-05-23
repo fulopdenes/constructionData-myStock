0657cfa796cbed48db0b5d5f9398337390de326b
fulopdenes <denes.fulopp@gmail.com>
2023-05-23 22:52:18 +0200

    set proxy to 8080

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const proxy = {
-    target: 'https://mystock-backend.onrender.com',
+    target: 'http://localhost:8080',
     changeOrigin: true
 }
 
