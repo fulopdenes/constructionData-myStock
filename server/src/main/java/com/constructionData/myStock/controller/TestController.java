a0c4b41386e9fc08ead12e0fb1cee38deff0b739
fulopdenes <denes.fulopp@gmail.com>
2023-05-24 16:23:40 +0200

    add crossOriginSetup

--- server/src/main/java/com/constructionData/myStock/controller/TestController.java
+++ server/src/main/java/com/constructionData/myStock/controller/TestController.java
@@ -1,10 +1,12 @@
 package com.constructionData.myStock.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "https://mystock-backend.onrender.com")
 @RequestMapping("/api")
 //@CrossOrigin(origins = {"http://192.168.0.17:3000", "http://localhost:3000"})
 public class TestController {
--- server/src/main/java/com/constructionData/myStock/controller/UserController.java
+++ server/src/main/java/com/constructionData/myStock/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "https://mystock-backend.onrender.com")
 @RequestMapping("/api/users")
 public class UserController {
 
