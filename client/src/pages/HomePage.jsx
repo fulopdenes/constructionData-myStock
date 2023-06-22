e287e715dbf7f9442f7c60ed1be3300330e2baa6
Fülöp Dénes <33910804+fulopdenes@users.noreply.github.com>
2023-06-22 13:46:33 +0200

    Update HomePage.jsx

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -8,10 +8,11 @@ const HomePage = () => {
                 This website works with a <b>free server service provider</b>, so the server does not run permanently.
                 To see OFFICE and ONSITE pages, after the first request sent you need to wait approximately <b>150 sec (2
                 minutes and 30 sec)</b>, then refresh the current page.
-                
-                So please click at the OFFICE page and wait cca. 150 sec, (you will see the circle loading around), then please refresh the page.
             </div>
             <p></p>
+            <div>
+               So, please click at the OFFICE page (you will see the blue circle loading symbol) and wait cca. 150 seconds then please refresh the page.
+            </div>
             <p></p>
             Copyright © Fülöp Dénes 2023
         </div>
