4312c5d48203abc37393a9d4b2c02fa92fcc17d1
fulopdenes <denes.fulopp@gmail.com>
2023-06-23 05:40:42 +0200

    add security

--- server/src/main/java/com/constructionData/myStock/controller/ProductController.java
+++ server/src/main/java/com/constructionData/myStock/controller/ProductController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "https://mystock-frontend.onrender.com")
+@CrossOrigin()
 @RequestMapping("/api/products")
 public class ProductController {
 
