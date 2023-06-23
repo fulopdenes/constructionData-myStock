39bb079c0bc5a32b0523a2908eb368ee9c028aa5
fulopdenes <denes.fulopp@gmail.com>
2023-06-23 06:06:51 +0200

    cross try

--- server/src/main/java/com/constructionData/myStock/MyStockApplication.java
+++ server/src/main/java/com/constructionData/myStock/MyStockApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.time.LocalDateTime;
 
@@ -18,15 +21,15 @@ public class MyStockApplication {
 
     //TODO: dataGeneration should be better organized separate.
 
-//    @Configuration
-//    public static class WebConfig implements WebMvcConfigurer {
-//        @Override
-//        public void addCorsMappings(CorsRegistry registry) {
-//            registry.addMapping("/**")
-//                    .allowedOrigins("*")
-//                    .allowedMethods("GET", "POST", "PUT", "DELETE");
-//        }
-//    }
+    @Configuration
+    public static class WebConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE");
+        }
+    }
 
 
     @Bean
